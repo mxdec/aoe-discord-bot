a1ea421da9103a9ebb23bef7242bdb44861514b7
Maxime de Courchelle <maxime.decourchelle@doctolib.com>
2024-11-11 20:35:53 +0100

    datasource: change source from aoe2.net to WorldsEdge API

--- src/aoe.py
+++ src/aoe.py
@@ -1,91 +1,56 @@
 # Standard Library
-import json
 import logging
 from dataclasses import dataclass
 from typing import List, Optional
 
 # Third Party
-import desert
-import websockets
+import requests
+
+@dataclass
+class ConfigPlayer:
+    """Represents the player in the config file."""
+
+    name: Optional[str] = None
+    profileId: Optional[int] = None
+    steamId: Optional[str] = None
 
 
 @dataclass
-class Player:
-    """Websocket object representing the Player."""
+class Profile:
+    """Represents the profile."""
 
-    avatar: Optional[str] = None
-    avatarfull: Optional[str] = None
-    avatarmedium: Optional[str] = None
-    civ: Optional[int] = None
-    civName: Optional[str] = None
-    civAlpha: Optional[int] = None
-    color: Optional[int] = None
-    countryCode: Optional[str] = None
-    name: Optional[str] = None
-    profileId: Optional[int] = None
-    rating: Optional[int] = None
-    rec: Optional[str] = None
-    slot: Optional[int] = None
-    slotType: Optional[int] = None
-    steamId: Optional[str] = None
-    team: Optional[int] = None
-    won: Optional[bool] = None
+    id: int
+    name: str
+    alias: str
+    personal_statgroup_id: int
+    xp: int
+    country: str
+
+
+@dataclass
+class Member:
+    """Represents the player in a match."""
+
+    profile: Optional[Profile]
+    civilization_id: int
+    newrating: int
+    oldrating: int
+    outcome: int
+    teamid: int
 
 
 @dataclass
 class Match:
-    """Websocket object representing the match.
+    """Represents the match."""
 
-    If the match is not finished yet, the keys 'finished' and 'closed'
-    are left empty. A unix timestamp is set otherwise.
-    """
-
-    active: bool
-    appId: int
-    averageRating: Optional[int]
-    cheats: bool
-    closed: Optional[int]
-    finished: Optional[int]
-    full: bool
-    fullTechTree: bool
-    gameType: str
-    gameTypeId: int
-    hasPassword: Optional[bool]
-    hidden: bool
-    id: str
-    lastSeen: Optional[int]
-    location: str
-    lockSpeed: bool
-    lockTeams: bool
-    mapSize: str
-    name: str
-    numPlayers: int
-    numSlots: int
-    numSpectators: int
-    players: List[Player]
-    pop: int
-    ranked: Optional[bool]
-    ratingType: Optional[int]
-    resources: str
-    server: Optional[str]
-    sharedExploration: bool
-    speed: str
-    started: int
-    startingAge: str
-    status: str
-    steamLobbyId: Optional[str]
-    turbo: bool
-    victory: str
-    visibility: str
-
-
-@dataclass
-class PlayerRecentMatches:
-    """Websocket object representing the player's last matches."""
-
-    data: List[Match]
-    message: str
-    id: str
+    id: int
+    mapname: str
+    matchtype_id: int
+    description: str
+    startgametime: int
+    completiontime: int
+    replay: Optional[str]
+    members: List[Member]
 
 
 @dataclass
@@ -93,113 +58,78 @@ class PlayerMatches:
     """Private class that combines the player with his recent matches."""
 
     matches: List[Match]
-    player: Player
+    steam_id: str
 
 
-class WSClient:
-    """The WS client for AoE2.net.
-
-    The following messages are allowed:
-    - {message: "playerprofile", id: <profile_id>})
-    - {message: "playerhistorychart", id: <profile_id>})
-    - {message: "playerleaderboardstats", id: <profile_id>})
-    - {message: "playerrecentmatches", id: <profile_id>})
-    - {message: "ping", data: <time_since_epoch_seconds>})
-
-    Example:
-        >>> ws = WSClient("wss://aoe2.net/ws")
-        >>> players = Player(steamId="123456789", profileId="12345678")
-        >>> matches = await ws.get_lastmatches(players, True)
-        >>> print(matches)
-    """
-
-    headers = {
-        "Origin": "https://www.aoe2.net",
-    }
+class WorldsEdgeApiClient:
+    """The HTTP client for World's Edge."""
 
     def __init__(self, url: str) -> None:
         self.url = url
 
-    async def get_matches(
-        self, players: List[Player]
-    ) -> Optional[List[PlayerMatches]]:
+    def get_matches(self, players: List[ConfigPlayer]) -> Optional[List[PlayerMatches]]:
+        """Performs the HTTP requests."""
         pms: List[PlayerMatches] = []
 
-        # connect to the server's socket
-        try:
-            ws = await websockets.connect(
-                uri=self.url, ssl=True, extra_headers=self.headers
-            )
-        except Exception as e:
-            logging.error(f"unable to connect to {self.url}: {e}")
-            return
-
-        logging.info("socket connected")
-        # request the last matches for each player
         for pl in players:
-            logging.info(f"getting {pl.name} (id: {pl.profileId}) matches...")
-            try:
-                await ws.send(
-                    json.dumps(
-                        {
-                            "message": "playerrecentmatches",
-                            "id": str(pl.profileId),
-                        }
-                    )
+            logging.info(f"getting {pl.name} (id: {pl.steamId}) matches...")
+
+            resp = requests.get(f'{self.url}/community/leaderboard/getRecentMatchHistory?title=age2&profile_names=[%22/steam/{pl.steamId}%22]')
+            if resp.status_code != 200:
+                logging.error(f"HTTP request error with status: {resp.status_code}")
+                return None
+
+            data = resp.json()
+            matches = data['matchHistoryStats']
+            profiles = [
+                Profile(
+                    id=profile['profile_id'],
+                    name=profile['name'],
+                    alias=profile['alias'],
+                    personal_statgroup_id=profile['personal_statgroup_id'],
+                    xp=profile['xp'],
+                    country=profile['country'],
                 )
-            except Exception as e:
-                logging.error(f"error while sending msg: {e}")
-                break
+                for profile in data['profiles']
+            ]
 
-            retry = 3
-            while retry > 0:
-                retry -= 1
-                try:
-                    raw = await ws.recv()
-                    resp = json.loads(raw)
-                    if (
-                        "data" in resp
-                        and isinstance(resp["data"], list)
-                        and len(resp["data"]) > 0
-                    ):
-                        prm = desert.schema(PlayerRecentMatches).load(resp)
-                        # remove empty slots from player list
-                        for data in prm.data:
-                            data.players = [
-                                pl
-                                for pl in data.players
-                                if pl.name is not None
-                            ]
-                        pms.append(PlayerMatches(player=pl, matches=prm.data))
-                        break
-                    else:
-                        logging.info(f"ignoring message: {raw}, {retry} left")
-                except Exception as e:
-                    logging.error(
-                        f"error while getting {pl.name} matches: {e}"
+            parsedMatches = []
+            for match in matches:
+                matchMembers = [
+                    Member(
+                        profile=self.find_member_profile(profiles, member['profile_id']),
+                        civilization_id=member['civilization_id'],
+                        teamid=member['teamid'],
+                        outcome=member['outcome'],
+                        oldrating=member['oldrating'],
+                        newrating=member['newrating']
                     )
+                    for member in match['matchhistorymember']
+                ]
 
-        await ws.close()
-        logging.info("socket closed")
+                parsedMatch = Match(
+                    id=match['id'],
+                    mapname=match['id'],
+                    matchtype_id=match['matchtype_id'],
+                    description=match['description'],
+                    startgametime=match['startgametime'],
+                    completiontime=match['completiontime'],
+                    members=matchMembers,
+                    replay=self.get_replay(match['matchurls']),
+                )
+                parsedMatches.append(parsedMatch)
 
-        # log results
-        msg = f"found matches for {len(pms)}/{len(players)} players"
-        if len(pms) != len(players):
-            logging.error(msg)
-            return
+            pms.append(PlayerMatches(steam_id=pl.steamId, matches=parsedMatches))
 
-        logging.info(msg)
+        logging.info(f"found matches for {len(pms)}/{len(players)} players")
         return pms
 
-    async def get_lastmatches(
-        self, players: List[Player]
-    ) -> Optional[List[Match]]:
-        """Gather last finished match for each player."""
+    def get_lastmatches(self, players: List[ConfigPlayer]) -> Optional[List[Match]]:
+        """Gathers last finished matchs for each player."""
         matches: List[Match] = []
         dedups: List[Match] = []
 
-        # connect to the server's socket
-        pms = await self.get_matches(players)
+        pms = self.get_matches(players)
         if pms is None:
             return None
 
@@ -214,3 +144,17 @@ class WSClient:
             dedups.append(match)
 
         return dedups
+
+    def find_member_profile(self, profiles: List[Profile], profile_id: int) -> Optional[Profile]:
+        """Finds the profile by ID."""
+        filtered_profiles = [profile for profile in profiles if profile.id == profile_id]
+
+        if len(filtered_profiles) == 0: return None
+
+        return filtered_profiles[0]
+
+    def get_replay(self, match_urls: List[object]) -> None:
+        """Extract the replay link."""
+        if len(match_urls) == 0: return None
+
+        return match_urls[0]['url']
